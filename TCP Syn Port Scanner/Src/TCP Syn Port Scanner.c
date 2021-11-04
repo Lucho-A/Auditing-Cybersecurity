400f0781f1a5210df895b94e4394c6fc6bdb593d
Lucho-A <lucho@lucho-L>
2021-11-04 20:37:40 -0300

    A ver ahora...

--- TCP Syn Port Scanner/Src/TCP Syn Port Scanner.c
+++ TCP Syn Port Scanner/Src/TCP Syn Port Scanner.c
@@ -12,6 +12,6 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	puts("A ver ahora..."); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
