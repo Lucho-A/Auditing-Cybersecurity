ffbf62d5860802b378ecb98eabad11e9aee8cab7
Lucho-A <lucho@lucho-L>
2021-11-04 20:44:51 -0300

    v1.0.3

---
+++ TCP Syn Port Scanner/Src/Headers/TCPSynPortScanner.h
@@ -0,0 +1,55 @@
+/*
+ ============================================================================
+ Name        : TCP Syn Port Scanner.h
+ Author      : L.
+ Version     : 1.0.3
+ Copyright   : GNU General Public License v3.0
+ Description : Port Scanner in C, Ansi-style
+ ============================================================================
+*/
+
+#ifndef HEADERS_TCPSYNPORTSCANNER_H_
+#define HEADERS_TCPSYNPORTSCANNER_H_
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<errno.h>
+#include<pthread.h>
+#include<netdb.h>
+#include<arpa/inet.h>
+#include<netinet/tcp.h>
+#include<netinet/ip.h>
+#include<time.h>
+#include <unistd.h>
+
+#define TRUE 1
+#define FALSE 0
+#define RED "\e[0;91m"
+#define CYAN "\e[0;36m"
+#define WHITE "\e[0;37m"
+#define YELLOW "\e[0;93m"
+#define DEFAULT "\e[0m"
+#define CANT_PORTS 5000
+static const long RETURN_OK;
+
+struct pseudo_header{
+	unsigned int source_address;
+	unsigned int dest_address;
+	unsigned char placeholder;
+	unsigned char protocol;
+	unsigned short tcp_length;
+	struct tcphdr tcp;
+};
+
+struct in_addr dest_ip;
+
+void * receive_ack( void *ptr );
+void process_packet(unsigned char* , int);
+unsigned short csum(unsigned short * , int );
+char * hostname_to_ip(char * );
+void get_local_ip (char *);
+int start_sniffer();
+
+#endif /* HEADERS_TCPSYNPORTSCANNER_H_ */
